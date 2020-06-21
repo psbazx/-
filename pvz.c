09c1db00c34d5377e3e94edfa6c3384f1c319c6a
皮三宝 <43060123+psbazx@users.noreply.github.com>
2020-06-21 12:47:59 +0800

    Update pvz.c

--- pvz.c
+++ pvz.c
@@ -1,22 +1,78 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<windows.h>
+#include <stdio.h>   
+#include <stdint.h>
+#include <pthread.h>  
+#include <time.h>
+#include <windows.h>
+#include <string.h>
 #include <tlhelp32.h>
+#pragma comment(lib, "pthreadVC2.lib")
 
+unsigned char checksign = 0;
+unsigned char checksign2 = 0;
 int find_process();
+DWORD WINAPI nocd(HANDLE phandle);
 int main()
 {
 	int ppid = find_process();
 	HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, false, ppid);
+	HANDLE hThread1;
+	DWORD EXITCODE;
 	unsigned int sun;
+	unsigned int coin;
+	unsigned int nopcode = 0x90;
+	unsigned int origincode[2] = { 0x75 ,0x5F };
+	unsigned char choice;
 	while (true)
 	{
-		printf("输入阳光值:");
-		scanf("%d", &sun);
-		DWORD addr;
-		ReadProcessMemory(phandle, (void*)(0x331C50+0x400000), &addr, sizeof(DWORD), NULL);
-		ReadProcessMemory(phandle, (void*)(addr + 0x868), &addr, sizeof(DWORD), NULL);
-		WriteProcessMemory(phandle, (void*)(addr + 0x5578), &sun, sizeof(DWORD), NULL);
+		printf("1.修改阳光值\n");
+		printf("2.无CD时间\n");
+		printf("3.终极大嘴花\n");
+		printf("4.修改金币\n");
+		printf("plz input your choice:");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf("请输入阳光值:");
+			scanf("%d", &sun);
+			DWORD addr;
+			ReadProcessMemory(phandle, (void*)(0x331C50 + 0x400000), &addr, sizeof(DWORD), NULL);
+			ReadProcessMemory(phandle, (void*)(addr + 0x868), &addr, sizeof(DWORD), NULL);
+			WriteProcessMemory(phandle, (void*)(addr + 0x5578), &sun, sizeof(DWORD), NULL);
+			break;
+		case 2:
+			checksign ^= 1;
+			if (checksign)
+			{
+				hThread1 = ::CreateThread(NULL, 0, nocd, phandle, 0, NULL);
+			}
+			break;
+		case 3:
+			checksign2 ^= 1;
+			if (checksign2)
+			{
+				WriteProcessMemory(phandle, (void*)(0x467905), &nopcode, sizeof(BYTE), NULL);
+				WriteProcessMemory(phandle, (void*)(0x467906), &nopcode, sizeof(BYTE), NULL);
+			}
+			else
+			{
+				WriteProcessMemory(phandle, (void*)(0x467905), &origincode[0], sizeof(BYTE), NULL);
+				WriteProcessMemory(phandle, (void*)(0x467906), &origincode[1], sizeof(BYTE), NULL);
+			}
+			break;
+		case 4:
+			printf("请输入金币值(金币最小单位为10):");
+			scanf("%d", &coin);
+			DWORD addr2;
+			ReadProcessMemory(phandle, (void*)(0x331C50 + 0x400000), &addr2, sizeof(DWORD), NULL);
+			ReadProcessMemory(phandle, (void*)(addr2 + 0x94C), &addr2, sizeof(DWORD), NULL);
+			WriteProcessMemory(phandle, (void*)(addr2 + 0x54), &coin, sizeof(DWORD), NULL);
+			break;
+		default:
+			printf("error input\n");
+			break;
+		}
 	}
 	return 0;
 }
@@ -42,5 +98,25 @@ int find_process()
 			}
 			flag = Process32Next(hSnapShot, &stProcess);
 		} while (flag);
+	}
+}
+DWORD WINAPI nocd(HANDLE phandle)
+{
+	DWORD addr2;
+	int i;
+	unsigned int cd = 1;
+	while (true)
+	{
+		if (!checksign)
+		{
+			return 0;
+		}
+		for (i = 0; i < 10; i++)
+		{
+			ReadProcessMemory(phandle, (void*)(0x331C50 + 0x400000), &addr2, sizeof(DWORD), NULL);
+			ReadProcessMemory(phandle, (void*)(addr2 + 0x868), &addr2, sizeof(DWORD), NULL);
+			ReadProcessMemory(phandle, (void*)(addr2 + 0x15C), &addr2, sizeof(DWORD), NULL);
+			WriteProcessMemory(phandle, (void*)(addr2 + 0x70 + 0x50*i), &cd, sizeof(DWORD), NULL);
+		}
 	}
 }
